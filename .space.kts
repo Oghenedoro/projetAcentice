29a4931d3d9ccdbae7acc422849b68568e74a2d2
Christian Ngouffo <christdana14@gmail.com>
2021-05-01 11:51:40 +0000

    Setting up automation script

---
+++ .space.kts
@@ -0,0 +1,37 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build and run tests") {
+    container(displayName = "Run mvn install", image = "maven:latest") {
+        shellScript {
+            content = """
+	            mvn clean install
+                cp target/*.jar mnt/space/share
+            """
+        }
+    }
+}
+
+job("Build and push Docker") {
+    docker {
+        beforeBuildScript {
+            content = """
+                export BRANCH=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'/' -f 3)
+            	mkdir target
+            	cp mnt/space/share/*.jar target/
+            """
+        }
+        build {
+            context = "."
+            file = "./Dockerfile"
+            labels["vendor"] = "Acentice"
+        }
+
+        push("acentice.registry.jetbrains.space/ideale/idealeapi") {
+            tag = "version-0.\$JB_SPACE_EXECUTION_NUMBER-\$BRANCH"
+        }
+    }
+}
