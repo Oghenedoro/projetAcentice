814036e4b8a49d6b74ddfdd994fcd9271e52418d
Oghenedoro <34007439+Oghenedoro@users.noreply.github.com>
2021-05-15 00:39:43 +0200

    first commit

---
+++ src/main/java/com/org/acen/appAcentice/AppAcenticeApplication.java
@@ -0,0 +1,13 @@
+package com.org.acen.appAcentice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAcenticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAcenticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/acen/appAcentice/AppAcenticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.acen.appAcentice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAcenticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
